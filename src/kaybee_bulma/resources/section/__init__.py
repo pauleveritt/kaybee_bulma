89839205bd11a295acb3a4d6804dd7f0571e1ae2
Paul Everitt <pauleveritt@me.com>
2017-11-10 16:00:26 -0500

    Sections have a model for a featured article and a helper that can go get that model.

--- src/kaybee_bulma/resources/section/__init__.py
+++ src/kaybee_bulma/resources/section/__init__.py
@@ -4,9 +4,16 @@ from kaybee.resources.base import BaseResource, BaseContainerModel
 
 class SectionModel(BaseContainerModel):
     subheading: str = None
+    featured_docname: str = None
 
 
 @registry.resource('section')
 class Section(BaseResource):
     model = SectionModel
-    pass
+
+    def featured_resource(self, site):
+        fd = self.props.featured_docname
+        if not fd:
+            return None
+        else:
+            return site.resources[fd]
--- tests/unit/resources/test_section.py
+++ tests/unit/resources/test_section.py
@@ -3,10 +3,29 @@ import pytest
 from kaybee_bulma.resources.section import Section
 
 
+class DummyArticle:
+    title = 'Dummy Article 1'
+    docname = 'articles/article1'
+    published = 'somedate'
+
+
+class DummySite:
+    def __init__(self):
+        self.resources = dict()
+
+
 @pytest.fixture()
-def dummy_section():
+def dummy_section_default():
     content = """
 subheading: some subheading text
+    """
+    yield Section('s1', 'dummysection', content)
+
+
+@pytest.fixture()
+def dummy_section_feature():
+    content = """
+featured_docname: articles/article1
     """
     yield Section('s1', 'dummysection', content)
 
@@ -22,16 +41,46 @@ overrides:
     yield Section('s1', 'dummysection', content)
 
 
-def test_import():
-    assert Section.__name__ == 'Section'
+@pytest.fixture()
+def dummy_article():
+    yield DummyArticle()
 
 
-def test_construction(dummy_section):
-    assert dummy_section.name == 's1'
-    assert dummy_section.kbtype == 'dummysection'
-    assert dummy_section.props.subheading == 'some subheading text'
+@pytest.fixture()
+def dummy_site(dummy_article):
+    site = DummySite()
+    site.resources[dummy_article.docname] = dummy_article
+    yield site
 
 
-def test_dummy_override(dummy_override):
-    dao = dummy_override.props.overrides['article']['template']
-    assert 'override_article.html' == dao
+class TestSection:
+    def test_import(self):
+        assert Section.__name__ == 'Section'
+
+    def test_construction(self, dummy_section_default):
+        assert dummy_section_default.name == 's1'
+        assert dummy_section_default.kbtype == 'dummysection'
+        assert dummy_section_default.props.subheading == 'some subheading text'
+
+    def test_dummy_override(self, dummy_override):
+        dao = dummy_override.props.overrides['article']['template']
+        assert 'override_article.html' == dao
+
+    def test_no_featured_docname(self, dummy_section_default: Section):
+        assert None is dummy_section_default.props.featured_docname
+
+    def test_featured_docname(self, dummy_section_feature: Section):
+        assert 'articles/article1' == \
+               dummy_section_feature.props.featured_docname
+
+    def test_no_featured_resource(self, dummy_section_default: Section,
+                                  dummy_site: DummySite):
+        assert None is dummy_section_default.props.featured_docname
+        fr = dummy_section_default.featured_resource(dummy_site)
+        assert None is fr
+
+    def test_featured_resource(self, dummy_section_feature: Section,
+                               dummy_site: DummySite,
+                               dummy_article: DummyArticle):
+        fr = dummy_section_feature.featured_resource(dummy_site)
+        assert dummy_article.docname == fr.docname
