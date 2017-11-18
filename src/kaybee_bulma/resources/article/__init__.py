1dcd05e18f709ab2d2fc90a8b0607a25681b9298
Paul Everitt <pauleveritt@me.com>
2017-11-18 11:47:06 -0500

    Series are now looking better.

--- src/kaybee_bulma/resources/article/__init__.py
+++ src/kaybee_bulma/resources/article/__init__.py
@@ -9,21 +9,3 @@ class ArticleModel(BaseResourceModel):
 @registry.resource('article')
 class Article(BaseResource):
     model = ArticleModel
-
-    def series(self, site):
-        parent = site.resources[self.parent]
-        results = []
-        for docname in parent.toctree:
-            resource = site.resources.get(docname)
-            if resource:
-                # We might have a non-resource page in the toctree,
-                # so skip it if true
-                excerpt = getattr(resource.props, 'excerpt', False)
-                results.append(
-                    dict(
-                        docname=docname,
-                        title=resource.title,
-                        excerpt=excerpt
-                    )
-                )
-        return results
