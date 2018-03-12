6339a91afde3ca5e9f753b3c202ca7e87f0038be
Paul Everitt <pauleveritt@me.com>
2018-03-12 09:19:20 -0400

    @WIP Let's make a release.

--- kaybee_bulma/__init__.py
+++ kaybee_bulma/__init__.py
@@ -3,7 +3,7 @@ import os
 import kaybee_bulma.events
 from kaybee_bulma.siteconfig import SiteConfig
 
-__version__ = "0.2.12.dev0"
+__version__ = "0.2.12"
 __title__ = "kaybee_bulma"
 __description__ = "Bulma-based theme for Kaybee"
 __uri__ = "https://github.com/pauleveritt/kaybee_bulma"
