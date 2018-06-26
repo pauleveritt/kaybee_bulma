2520d4563de2d988d867cbc45875d9f06f9fba07
Paul Everitt <pauleveritt@me.com>
2018-06-26 10:21:21 -0400

    Let's make a release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='kaybee_bulma',
-    version='0.2.22',
+    version='0.2.23.dev0',
     description='Bulma-based theme for Kaybee',
     long_description=long_description,
     url='https://github.com/pauleveritt/kaybee_bulma',
