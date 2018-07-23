d33b139d8b10117f0f3572897b789af40099a635
Paul Everitt <pauleveritt@me.com>
2018-07-23 09:22:57 -0400

    The learn page is in place.

--- docs/kaybee_plugins/kbblearn.py
+++ docs/kaybee_plugins/kbblearn.py
@@ -5,7 +5,7 @@ from kaybee.plugins.articles.base_article import BaseArticleModel, BaseArticle
 
 content = load('''
 sidebar:
-  - label: Learn
+  - label: Dashboard
     href: /learn.html
   - label: Technologies
     is_active: true
@@ -31,6 +31,39 @@ sidebar:
   - label: IDEs
   - label: Resources
   - label: Authors
+
+sections:
+    - label: Dashboard
+      subheading:  An overview of useful sublistings... new, favorites, etc.
+      href: /documentation_overview.html
+      accent: primary
+      icon: fas fa-eye
+    - label: Technologies
+      subheading: Popular libraries and frameworks used in Python development 
+      href: /documentation_overview.html
+      accent: start
+      icon: fas fa-paint-brush
+    - label: Topics
+      subheading: Techniques and tools used to get into the PyCharm flow
+      href: /documentation_overview.html
+      accent: grey
+      icon: fas fa-cogs
+    - label: IDEs
+      subheading: Browse content organized by all the JetBrains integrated 
+        development environments
+      href: /documentation_overview.html
+      accent: star
+      icon: fas fa-columns
+    - label: Resources
+      subheading: All the kinds of content... tutorials, tips, videos, and more.
+      href: /documentation_overview.html
+      accent: success
+      icon: fas fa-warehouse
+    - label: Authors
+      subheading: Navigate the Guide content by the person who wrote it
+      href: /documentation_overview.html
+      accent: link
+      icon: fab fa-wpforms
 ''', Loader=Loader)
 
 
@@ -48,3 +81,7 @@ class KbbLearnResource(BaseArticle):
     @property
     def sidebar_entries(self):
         return content['sidebar']
+
+    @property
+    def section_entries(self):
+        return content['sections']
