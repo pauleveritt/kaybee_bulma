caf859b277860a038e6d109852c0dbbcf50f4285
Paul Everitt <pauleveritt@me.com>
2018-08-09 08:37:39 +0200

    Get rid of some more unused YAML.

--- docs/kaybee_plugins/kbbtopic.py
+++ docs/kaybee_plugins/kbbtopic.py
@@ -5,44 +5,6 @@ from kaybee.plugins.articles.base_article_reference import \
 )
 from kaybee.plugins.queries.service import Query
 
-from ruamel.yaml import load, Loader
-
-content = load('''
-sections:
-    - label: Angular
-      subheading:  An overview of useful sublistings... new, favorites, etc.
-      href: /documentation_overview.html
-      accent: primary
-      icon: fas fa-eye
-    - label: Django
-      subheading: Popular libraries and frameworks used in Python development 
-      href: /documentation_overview.html
-      accent: start
-      icon: fas fa-paint-brush
-    - label: Docker
-      subheading: Techniques and tools used to get into the PyCharm flow
-      href: /documentation_overview.html
-      accent: grey
-      icon: fas fa-cogs
-    - label: Flask
-      subheading: Browse content organized by all the JetBrains integrated 
-        development environments
-      href: /documentation_overview.html
-      accent: star
-      icon: fas fa-columns
-    - label: TypeScript
-      subheading: All the kinds of content... tutorials, tips, 
-        videos, and more.
-      href: /documentation_overview.html
-      accent: success
-      icon: fas fa-warehouse
-    - label: Vagrant
-      subheading: Navigate the Guide content by the person who wrote it
-      href: /documentation_overview.html
-      accent: link
-      icon: fab fa-wpforms
-''', Loader=Loader)
-
 
 class KbbTopicModel(BaseArticleReferenceModel):
     website: str
