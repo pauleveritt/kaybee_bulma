2768d9cbd9aaaf1c32fa5b2fe21181192c9ca619
Paul Everitt <pauleveritt@me.com>
2018-08-09 08:25:09 +0200

    No longer using hardwired YAML.

--- docs/kaybee_plugins/kbblearn.py
+++ docs/kaybee_plugins/kbblearn.py
@@ -1,45 +1,8 @@
-from kaybee.plugins.queries.service import Query
-from ruamel.yaml import load, Loader
-
 from kaybee.app import kb
 from kaybee.plugins.articles.base_article import BaseArticleModel, BaseArticle
+from kaybee.plugins.queries.service import Query
 
 from docs.kaybee_plugins.shared import sidebar
-
-content = load('''
-sections:
-    - label: Dashboard
-      subheading:  An overview of useful sublistings... new, favorites, etc.
-      docname: /learn/dashboard
-      accent: primary
-      icon: fas fa-eye
-    - label: Technologies
-      subheading: Popular libraries and frameworks used in Python development 
-      docname: /technologies/index
-      accent: start
-      icon: fas fa-paint-brush
-    - label: Topics
-      subheading: Techniques and tools used to get into the PyCharm flow
-      docname: /technologies/index
-      accent: grey
-      icon: fas fa-cogs
-    - label: IDEs
-      subheading: Browse content organized by all the JetBrains integrated 
-        development environments
-      docname: /technologies/index
-      accent: star
-      icon: fas fa-columns
-    - label: Resources
-      subheading: All the kinds of content... tutorials, tips, videos, and more.
-      docname: /technologies/index
-      accent: success
-      icon: fas fa-warehouse
-    - label: Authors
-      subheading: Navigate the Guide content by the person who wrote it
-      docname: /technologies/index
-      accent: link
-      icon: fab fa-wpforms
-''', Loader=Loader)
 
 
 class KbbLearnModel(BaseArticleModel):
