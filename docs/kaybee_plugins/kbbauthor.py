85d7ef84f6f5df0ecb8d1c8106d8208cf24c8768
Paul Everitt <pauleveritt@me.com>
2018-08-12 14:54:07 +0200

    Author pages work.

--- docs/kaybee_plugins/kbbauthor.py
+++ docs/kaybee_plugins/kbbauthor.py
@@ -4,4 +4,22 @@ from kaybee.plugins.articles.author import Author
 
 @kb.resource('kbbauthor')
 class KbbAuthorResource(Author):
-    pass
+    def section_entries(self, resources, references):
+        results = self.get_sources(resources)
+
+        return [
+            dict(
+                title=r.title,
+                rtype=r.rtype,
+                excerpt=r.excerpt,
+                docname=r.docname,
+                duration=r.props.duration,
+                published=r.props.published,
+                accent='primary',
+                icon='fas fa-eye',
+                author=r.get_author(references),
+                references=r.get_references(references),
+                logo=r.get_logo(resources)
+            )
+            for r in results
+        ]
